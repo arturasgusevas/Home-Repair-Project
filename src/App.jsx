403fcca57f29fd38ae0b3dfee5934363931fbcdd
Arturas Gusevas <arturas.gusevas@yahoo.com>
2024-09-17 20:12:05 +0300

    Add Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import TopBar from './components/TopBar/TopBar';
 import Search from './components/Search/Search';
+import CardList from './components/CardList/CardList';
 
 function App() {
   return (
     <div className='App'>
       <TopBar />
       <Search />
+      <CardList />
     </div>
   );
 }
