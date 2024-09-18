6ff4156fed0eb0f08d11cad2843ae98a33af06aa
Arturas Gusevas <arturas.gusevas@yahoo.com>
2024-09-18 17:10:30 +0300

    Fix: Remove JSX in cardData and update CardList for icon rendering

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -8,12 +8,12 @@ import {
 } from 'react-icons/md';
 
 const cardData = [
-  {id: 1, icon: <MdOutlineCleaningServices />, title: 'Cleaning'},
-  {id: 2, icon: <MdBuild />, title: 'Repair'},
-  {id: 3, icon: <MdBrush />, title: 'Painting'},
-  {id: 4, icon: <MdLocalShipping />, title: 'Shifting'},
-  {id: 5, icon: <MdPlumbing />, title: 'Plumbing'},
-  {id: 6, icon: <MdOutlineElectricBolt />, title: 'Electrical'}
+  {id: 1, icon: MdOutlineCleaningServices, title: 'Cleaning'},
+  {id: 2, icon: MdBuild, title: 'Repair'},
+  {id: 3, icon: MdBrush, title: 'Painting'},
+  {id: 4, icon: MdLocalShipping, title: 'Shifting'},
+  {id: 5, icon: MdPlumbing, title: 'Plumbing'},
+  {id: 6, icon: MdOutlineElectricBolt, title: 'Electrical'}
 ];
 
 export default cardData;
