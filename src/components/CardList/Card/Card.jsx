50158713e66ed3508e28e77ab952b5874c05720f
Arturas Gusevas <arturas.gusevas@yahoo.com>
2024-09-23 20:17:03 +0300

    Add useNavigate to handle card click

--- src/components/CardList/Card/Card.jsx
+++ src/components/CardList/Card/Card.jsx
@@ -1,9 +1,14 @@
+import {useNavigate} from 'react-router-dom';
 import styles from './Card.module.scss';
 import PropTypes from 'prop-types';
 
 function Card({icon, title}) {
+  const navigate = useNavigate();
+  const handleCardClick = () => {
+    navigate(`/search/${title.toLowerCase().replace(/\s+/g, '-')}`);
+  };
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleCardClick}>
       <div className={styles.icon}>{icon}</div>
       <h2 className={styles.title}>{title}</h2>
     </div>
