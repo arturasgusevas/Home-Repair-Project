a4f5e101e9b0e6e1cc4933b406ae74bebf96249a
Arturas Gusevas <arturas.gusevas@yahoo.com>
2024-09-29 15:40:22 +0300

    Fix card list layout issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import Search from '../components/Search/Search';
-import CardList from '../components/CardList/CardList';
 
 function Home() {
   return (
     <div>
       <Search />
-      <CardList />
     </div>
   );
 }
