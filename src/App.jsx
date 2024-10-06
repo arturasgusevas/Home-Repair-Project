1d9d93bda6d13688e8d3c8a36790fda9c9c36bdf
Arturas Gusevas <arturas.gusevas@yahoo.com>
2024-10-06 15:45:26 +0300

    Update App with UserProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import TopBar from './components/TopBar/TopBar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {UserProvider} from './context/UserContext';
 import Home from './pages/Home/Home';
 import Services from './pages/Services';
 import AboutUs from './pages/AboutUs';
@@ -9,18 +10,20 @@ import SearchCategory from './pages/SearchCategory';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <TopBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/services' element={<Services />} />
-          <Route path='/about-us' element={<AboutUs />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/search/:category' element={<SearchCategory />} />
-        </Routes>
-      </div>
-    </Router>
+    <UserProvider>
+      <Router>
+        <div className='App'>
+          <TopBar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/about-us' element={<AboutUs />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/search/:category' element={<SearchCategory />} />
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
   );
 }
 
