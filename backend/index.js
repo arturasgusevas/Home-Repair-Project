5111aed309573ef773cc8bb57f8f64de9159acf2
Arturas Gusevas <arturas.gusevas@yahoo.com>
2024-10-09 20:11:46 +0300

    Update index.js with authentication routes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const connectDB = require('./config/db');
 const categoryRoutes = require('./routes/categoryRoutes');
 const businessRoutes = require('./routes/businessRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
+const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
+const authMiddleware = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -16,10 +19,14 @@ connectDB();
 app.use('/categories', categoryRoutes);
 app.use('/businesses', businessRoutes);
 app.use('/bookings', bookingRoutes);
+app.use('/users', authMiddleware, userRoutes);
+app.use('/auth', authRoutes);
+
 
 app.use((err, req, res, next) => {
   res.status(500).json({message: err.message});
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
